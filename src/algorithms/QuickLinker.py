3bc889155120e4bb6fc743ddbaa9356544ed0e0d
Mitchell Wagner <mitchw94@vt.edu>
2018-04-02 00:34:57 -0400

    Fix algorithm name swap

--- src/algorithms/QuickLinker.py
+++ src/algorithms/QuickLinker.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import subprocess
 from pathlib import Path
-import sys
+
 from .RankingAlgorithm import RankingAlgorithm
 import src.external.pathlinker.parse as pl_parse
 import src.external.pathlinker.PathLinker as pl
@@ -19,24 +19,25 @@ class QuickLinker(RankingAlgorithm):
     Written generally, so no constraints on the regular expressions or
     the order they are provided are enforced.
     
-    Ranks edges based on edge RWR weights instead of path lengths.
-
+    Ranks edges based on iteractome edge weights instead of path lengths.
     '''
     def __init__(self, params):
         self.rlc_abbr = params["rlcs"][0]
         self.rlcs = params["rlcs"][1]
-        self.q = params["q"]
 
 
     def run(self, reconstruction_input):
-        # 1) Label interactome
-        # 2) Cut the unnecessary column out
-        # 3) Source Python2 venv
-        # 4) Call Aditya's code to generate DFA graph
-        # 5) Run the compiled Java binary
-        
-        #######################################################################
-        # 1)
+        # Read in the interactome
+        net = None
+        with reconstruction_input.interactome.open('r') as f:
+            net = pl.readNetworkFile(f)
+
+        weights = {}
+
+        for edge in net.edges(data=True):
+            #print(edge[2]["weight"])
+            weights[(edge[0], edge[1])] = edge[2]["weight"]
+
         provided_edges = reconstruction_input.training_edges 
         negatives = reconstruction_input.training_negatives
 
@@ -53,58 +54,6 @@ class QuickLinker(RankingAlgorithm):
 
             reconstruction_input.label_interactome_file(
                 in_file, out_file, sets, default="x")
-        # Read in the interactome
-        net = None
-        netCopy = None
-        with reconstruction_input.interactome.open('r') as f:
-            net = pl.readNetworkFile(f)
-
-        with reconstruction_input.interactome.open('r') as f:
-            netCopy = pl.readNetworkFile(f)
-
-        # Add dummy nodes for every node in the "head" of a p-labeled edge
-        TempNodes = set([])
-        for edge in provided_edges:
-            TempNodes.add(str(edge[0]+"_temp"))
-            netCopy.add_edge(str(edge[0]+"_temp"),edge[1],attr_dict=net.get_edge_data(edge[0],edge[1]))
-
-        # Restart to newly added temporary nodes
-        #weights = {node:1 for node in TempNodes}
-        weights = {} 
-        for edge in provided_edges:
-            # Default value of 0
-            weights[str(edge[0]+"_temp")] = weights.get(str(edge[0]+"_temp"), 0) + 1
-
-
-        # Set a minimum edge weight
-        for edge in net.edges(data=True):
-            if edge[2]["weight"] == 0:
-                edge[2]["weight"] = sys.float_info.min
-
-        # Set a minimum edge weight
-        for edge in netCopy.edges(data=True):
-            if edge[2]["weight"] == 0:
-                edge[2]["weight"] = sys.float_info.min
-    
-        # 1) PageRank using weighted restart set
-        pagerank_scores_weighted = pr.pagerank(
-            netCopy, weights=weights, q=float(self.q))
-
-        pl.calculateFluxEdgeWeights(netCopy, pagerank_scores_weighted)
-                
-        fluxes_weighted = {}
-        # Add edd fluxes computed from TempNodes to original head nodes
-        # If head is not in TempNodes, use normal ksp_weight
-        # If
-        for edge in netCopy.edges():
-            attr_dict=netCopy.get_edge_data(edge[0],edge[1])
-            if edge[0] in TempNodes:
-                attr_dict_original=netCopy.get_edge_data(edge[0][:-5],edge[1])
-                fluxes_weighted[(edge[0][:-5], edge[1])] = attr_dict_original["ksp_weight"]+attr_dict["ksp_weight"]
-            elif (edge[0],edge[1]) in provided_edges:
-                continue # This edge has already been added, do not overwrite it.
-            else:
-                fluxes_weighted[(edge[0], edge[1])] = attr_dict["ksp_weight"]
 
         #######################################################################
         # 2) Keep only the necessary columns
@@ -209,7 +158,7 @@ class QuickLinker(RankingAlgorithm):
             eWeights = [(
                 edge[0], 
                 edge[1], 
-                fluxes_weighted[(edge[0], edge[1])])
+                weights[(edge[0], edge[1])])
                 for edge in Edge_SPDict.keys()]
 
             # Sort the list of final scores 
@@ -239,6 +188,7 @@ class QuickLinker(RankingAlgorithm):
                             str(tup[2] + len(self.rlcs) - 1 - i) + "\n"]))
 
             prevRankMap = prevRankMap + len(rank_map.keys())
+
     def conform_output(self, output_dir):
         None
 
@@ -248,7 +198,7 @@ class QuickLinker(RankingAlgorithm):
 
 
     def get_descriptive_name(self):
-        return "QuickLinker, q=%s, rlcs=%s" % (self.q, self.rlc_abbr)
+        return "QuickLinker, rlcs=%s" % (self.rlc_abbr)
 
 
     def get_output_file(self):
@@ -258,4 +208,4 @@ class QuickLinker(RankingAlgorithm):
     def get_output_directory(self):
         return Path(    
             self.get_name(), 
-            "q-%s-rlcs-%s" % (self.q, self.rlc_abbr))
+            "rlcs-%s" % (self.rlc_abbr))
