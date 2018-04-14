e5d88f394da346b1548198114fd72a965d251c19
Mitchell Wagner <mitchw94@vt.edu>
2018-04-14 01:03:09 -0400

    Deactivate incorrect setup procedure

--- setup.bash
+++ setup.bash
@@ -30,30 +30,31 @@ pip install -r requirements-regpathlinker.txt
 deactivate
 
 ###############################################################################
+# TODO: This likely won't work unless this is actually run on baobab
 # Create Python virtual environment for running pipeline on baobab 
 # (local compute cluster)
 
-python3.6 -m venv baobab-venv
-
-source baobab-venv/bin/activate
-
-pip install -r requirements.txt
-
-deactivate
-
-# Create Python virtual environment for running algorithms in the pipeline
-# on baobab (local compute cluster)
-
-# NOTE: Right now, to run on baobab, you will have to manually change the
-# venv sourced in the code to run this algorithm...far less than ideal...
-
-python3.6 -m venv baobab-venv-regpathlinker -p python2
-
-source baobab-venv-regpathlinker/bin/activate
-
-pip install -r requirements-regpathlinker.txt
-
-deactivate
+#python3.6 -m venv baobab-venv
+#
+#source baobab-venv/bin/activate
+#
+#pip install -r requirements.txt
+#
+#deactivate
+#
+## Create Python virtual environment for running algorithms in the pipeline
+## on baobab (local compute cluster)
+#
+## NOTE: Right now, to run on baobab, you will have to manually change the
+## venv sourced in the code to run this algorithm...far less than ideal...
+#
+#python3.6 -m venv baobab-venv-regpathlinker -p python2
+#
+#source baobab-venv-regpathlinker/bin/activate
+#
+#pip install -r requirements-regpathlinker.txt
+#
+#deactivate
 
 ###############################################################################
 # Fetch the appropriate input from SVN
