af86c86d3b4b0d716b59ace28432e4fdce98a9ae
Mitchell Wagner <mitchw94@vt.edu>
2018-10-26 16:10:04 -0400

    tweak pip install

--- setup.bash
+++ setup.bash
@@ -6,8 +6,6 @@ git submodule update --init --recursive
 # Build gradle after downloading QuickLinker
 gradle build -p src/external/quicklinker/
 
-# TODO: Pypy!
-
 ###############################################################################
 # Create Python virtual environment for the pipeline
 
@@ -15,7 +13,7 @@ virtualenv venv -p python3
 
 source venv/bin/activate
 
-pip install -r requirements.txt
+pip install -r requirements/requirements.txt
 
 deactivate
 
@@ -25,36 +23,9 @@ virtualenv venv-regpathlinker -p python2
 
 source venv-regpathlinker/bin/activate
 
-pip install -r requirements-regpathlinker.txt
+pip install -r requirements/requirements-regpathlinker.txt
 
 deactivate
-
-###############################################################################
-# TODO: This likely won't work unless this is actually run on baobab
-# Create Python virtual environment for running pipeline on baobab 
-# (local compute cluster)
-
-#python3.6 -m venv baobab-venv
-#
-#source baobab-venv/bin/activate
-#
-#pip install -r requirements.txt
-#
-#deactivate
-#
-## Create Python virtual environment for running algorithms in the pipeline
-## on baobab (local compute cluster)
-#
-## NOTE: Right now, to run on baobab, you will have to manually change the
-## venv sourced in the code to run this algorithm...far less than ideal...
-#
-#python3.6 -m venv baobab-venv-regpathlinker -p python2
-#
-#source baobab-venv-regpathlinker/bin/activate
-#
-#pip install -r requirements-regpathlinker.txt
-#
-#deactivate
 
 ###############################################################################
 # Fetch the appropriate input from SVN
